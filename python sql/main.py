47881a946c83fc4b96dffc87f51a0a6c5380e954
Yuri Shornikov <dbonlyfun@gmail.com>
2023-01-15 23:51:17 +0300

    beginning

--- python sql/main.py
+++ python sql/main.py
@@ -6,4 +6,41 @@ with con.cursor() as cur:
     con.commit() # повторное создаине таблицы не произойдет, выдаст ошибку, комитит в конце
     con.rollback() #игнорит все изменения
 
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS genre
+        genre_id SERIAL PRIMARY KEY
+        name VARCHAR(60) NOT NULL
+        """)
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS musician
+        musician_id SERIAL PRIMARY KEY
+        name VARCHAR(60)
+        """)
+
+    cur.execute("""
+        CREATE TABLE MUSICIAN_genre
+        id SERIAL PRIMARY KEY
+        genre_id SERIAL NOT NULL references genre(genre_id)
+        musician_id SERIAL NOT NULL references musician(musician_id)
+        """)
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS album
+        album_id SERIAL PRIMARY KEY
+        name VARCHAR(60) NOT NULL
+        """)
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS musician_album
+        id SERIAL PRIMARY KEY
+        musician_id SERIAL NOT NULL references musician(musician_id)
+        album_id SERIAL NOY NULL references album(album_id
+        """)
+
+    cur.execute("""
+            CREATE TABLE IF NOT EXISTS """)
+
+
 con.close()
